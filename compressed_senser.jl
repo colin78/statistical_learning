0513cc5cbce68254f182901156a042a0ea352f44
Colin Pawlowski <colinpawlowski@Colins-MacBook-Pro.local>
2016-03-03 00:20:02 -0500

    added phase diagram csv for compressed senser

--- compressed_senser.jl
+++ compressed_senser.jl
@@ -1,4 +1,6 @@
+using DataFrames
 include("hw1_functions.jl")
+
 
 seed = 0
 m = 15
@@ -26,8 +28,17 @@ k = 14
 # 	end
 # end
 
-df_L0 = convert(DataFrame, nonzeros_L0)
-df_L1 = convert(DataFrame, nonzeros_L1)
+# df_L0 = convert(DataFrame, nonzeros_L0)
+# df_L1 = convert(DataFrame, nonzeros_L1)
 
-writetable("nonzeros_L0.csv", df_L0, header=false)
-writetable("nonzeros_L1.csv", df_L1, header=false)
+# writetable("nonzeros_L0.csv", df_L0, header=false)
+# writetable("nonzeros_L1.csv", df_L1, header=false)
+
+matrix_L0 = convert(Array, readtable("nonzeros_L0.csv", header=false))
+phase_L0 = 1*(matrix_L0 .== ones(Int8,20)*[1:20]')
+writetable("phase_L0.csv", convert(DataFrame, phase_L0), header=false)
+matrix_L1 = convert(Array, readtable("nonzeros_L1.csv", header=false))
+phase_L1 = 1*(matrix_L1 .== ones(Int8,20)*[1:20]')
+writetable("phase_L1.csv", convert(DataFrame, phase_L1), header=false)
+
+
